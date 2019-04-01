93503f53c829acac537775201c2c17605e6eca44
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-04-01 18:28:23 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <string.h>
 
 #include "./headers/card.h"
+#include "./headers/player.h"
 
 
+void printall(card cards[], int  max);
 
 int main(){
 
@@ -13,16 +15,41 @@ int main(){
   card cards[52];
   
   createCards(cards);
-  printf("--DECK CREATED\n\n");
+  printf("--DECK CREATED\n");
 
   shuffleCards(cards, 52);
-  printf("--DECK SHUFFLED\n\n");
+  printf("--DECK SHUFFLED\n");
 
-  dealCard(cards[0]);
-  dealCard(cards[1]);
-  dealCard(cards[2]);
-  dealCard(cards[3]);
-  dealCard(cards[4]);
+  printall(cards, 6);
+  
+  int playerCount = 2;
+  int handSize = 4;
+  int cardCount = 0;
+  player players[playerCount];
+  createPlayers(players, playerCount);
+  printf("--PLAYERS CREATED\n");
+  //printAllNames(players);
 
-  printf("--GAME END-----------------\n\n");
+  for(int i = 0; i < 4; i++){  
+  changeCard(players[0].hand, cards, i, i);
+  }
+    
+
+  
+  
+  printf("%d of %d \n", players[0].hand[0].face, players[0].hand[0].suit);
+  printf("%d of %d \n", players[0].hand[1].face, players[0].hand[1].suit);
+  printf("%d of %d \n", players[0].hand[2].face, players[0].hand[2].suit);
+  printf("%d of %d \n", players[0].hand[3].face, players[0].hand[3].suit);
+  printf("%d of %d \n", players[0].hand[4].face, players[0].hand[4].suit);
+  printf("%d of %d \n", players[0].hand[5].face, players[0].hand[5].suit);
+
+
+  printf("--GAME END-----------------\n");
+}
+
+void printall(card cards[], int  max){
+  for(int i = 0; i < max; i++){
+    printf("%d of %d \n", cards[i].face, cards[i].suit);
+  }
 }
