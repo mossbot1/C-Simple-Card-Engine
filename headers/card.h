a378fca0bec2c812c678791d6ce6210f986ce1da
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-04-02 10:02:52 +0000

    Update card.h

--- headers/card.h
+++ headers/card.h
@@ -1,44 +1,41 @@
 #ifndef CARD_H
 #define CARD_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 typedef struct {
   int suit;
+  int *pSuit;
   int face;
-}card;
+  int *pFace;
+}Card;
 
 #define N_SUITS 4
 #define N_NUMS 13
 
+//Make one card objects
+void card_create(Card *card, int suit, int face);
+
 //Make the deck of card objects
-void createCards(card cards[]);
-
-const char * printCardSuit(card thisCard);
-
-const char * printCardFace(card thisCard);
+void card_createAll(Card cards[]);
 
 //To string of cards (Two Of Hearts)
-void printCard(card thisCard);
+void card_print(Card card);
 
 //to string of card enum positions (0 Of 0)
-void printCardEnums(card thisCard);
+void card_printInt(Card card);
 
 //Fisher-Yates Shuffle
-void shuffleCards(card cards[], int n);
-
-//Add 1 to card dealt counter, return current cards dealt after;
-void dealCard();
+void card_shuffle(Card cards[], int n);
 
 //Used for counting how many of each suit is dealt.
-void suitCounter(card thisCard);
+void card_suitCount(Card card);
 
 //Used for counting how many of each face is dealt
-void faceCounter(card thisCard);
+void card_faceCount(Card card);
 
 //print current cards dealt
-void printCardsDealt();
+void card_dealtCount();
+
+//Add 1 to card dealt counter, return current cards dealt after;
+void card_deal(Card card);
 
 #endif
