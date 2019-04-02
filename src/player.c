dbfb5cba40a93d34dd42f6df266c6f70d38fcb28
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-04-02 10:01:26 +0000

    Update player.c
    Can print each card in players hand

--- src/player.c
+++ src/player.c
@@ -10,35 +10,55 @@ int cardCount = 0;
 
 const char nameStrings[][20] = {"Player", "Boris", "Sanchez", "Big Decky", "The Glorb", "Dustin", "Living Chair", "Derek", "Satan", "Ringo", "Gordon", "Jesus", "Swonk"};
 
-void createPlayers(player players[], int count){
+void player_create(Player *player){
+  player->id = playerCount;
+  player->name = playerCount;
+  playerCount++;
+}
+
+void player_createAll(Player players[], int count){
   for(int i = 0; i < count; i++){
-    players[i].id = i;
-    players[i].name = i;
-    playerCount++;
+    player_create(&players[i]);
   }
 }
 
+void player_printName(Player *player){
+   printf("%d. %s \n", player->id  ,nameStrings[player->name]);
 
-void printAllNames(player players[]){
+}
+
+void player_printAllNames(Player players[]){
   for(int i = 0; i < playerCount; i++){
-    printf("%d. %s \n", players[i].id  ,nameStrings[players[i].name]);
+    player_printName(&players[i]);
   }
 }
 
-void drawCards(player players[],  card cards[], int numPlayers, int handSize){
+void player_drawCard(Card *hand, Card *card){
+  hand->face = card->face;
+  hand->suit = card->suit;
+  
 
-  int cardCount = 0;
+}
+
+void player_drawAllCards(Player players[],  Card cards[], int numPlayers, int handSize){
   
   for(int i = 0; i < numPlayers; i++){
     for(int j = 0; j < handSize; j++){
-    players[i].hand[j].face = cards[cardCount].face;
-    players[i].hand[j].suit = cards[cardCount].suit;
-    cardCount++;
+      player_drawCard(&players[i].hand[j], &cards[cardCount]);
+      cardCount++;
     }
   }
 }
 
-void changeCard(card hand[], card cards[], int handpos,  int cardpos){
-  memcpy(&hand[handpos], &cards[cardpos], sizeof(card));
-  printf("thinks %d of %d \n ", cards[cardpos].face, cards[cardpos].suit);
-};
+void player_printCard(Player *player, int pos){
+  player_printName(player);
+  card_print(player->hand[pos]);
+  card_printInt(player->hand[pos]);
+}
+
+void player_printHand(Player *player, int handSize){
+  for(int i = 0; i < handSize; i++){
+    printf("(%d)\n", i);
+    player_printCard(player, i);
+  }
+}
