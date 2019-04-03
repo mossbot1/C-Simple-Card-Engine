5e48e7c55979421b28529b93e9652c217d0efc53
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-04-03 13:26:08 +0000

    Create global.h

---
+++ headers/global.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+//Variables for the global counting of players created, cards deakt and 
+
+#define N_SUITS 4
+#define N_FACES 13
+
+extern unsigned int playerCount;
+extern unsigned int cardCount;
+extern unsigned int cardsDealt;
+
+#endif
