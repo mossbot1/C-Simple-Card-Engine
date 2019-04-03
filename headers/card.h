5f07df13f8d02e2be5876b2bee355efaa39b95c3
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2019-04-03 13:27:48 +0000

    Update card.h

--- headers/card.h
+++ headers/card.h
@@ -8,34 +8,34 @@ typedef struct {
   int *pFace;
 }Card;
 
-#define N_SUITS 4
-#define N_NUMS 13
 
-//Make one card objects
+
+//Make 1 card object
 void card_create(Card *card, int suit, int face);
 
-//Make the deck of card objects
+//fill blank card array with card objects
 void card_createAll(Card cards[]);
 
-//To string of cards (Two Of Hearts)
+//print card (Two Of Hearts)
 void card_print(Card card);
 
-//to string of card enum positions (0 Of 0)
+//print card int positions (0 Of 0)
 void card_printInt(Card card);
 
-//Fisher-Yates Shuffle
+//Fisher-Yates Shuffle deck
 void card_shuffle(Card cards[], int n);
 
-//Used for counting how many of each suit is dealt.
-void card_suitCount(Card card);
+//increment suit counter array
+void card_suitCount(Card *card);
 
-//Used for counting how many of each face is dealt
-void card_faceCount(Card card);
+//increment face counter array
+void card_faceCount(Card *card);
 
-//print current cards dealt
-void card_dealtCount();
 
-//Add 1 to card dealt counter, return current cards dealt after;
-void card_deal(Card card);
+//Add 1 to card dealt counter as well as increment suits/faces counting arrays
+void card_deal(Card *card);
+
+//print cards dealt and suits dealt (debugging)
+void card_printDeals();
 
 #endif
